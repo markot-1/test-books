042c31e959ae09c398c77a42e3b94d8e152c6673
Marina Kotova <m.klementyeva@mail.ru>
2025-03-21 15:48:29 +0700

    create books-list component, add routes, install primeng

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BooksListComponent } from './components/books-list/books-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'books', component: BooksListComponent},
+    {path: '', redirectTo: 'books', pathMatch: 'full'}
+];
