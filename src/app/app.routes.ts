44558fb5e65e8fdb6bbaa9e7dec5f9dbc5807b16
Marina Kotova <m.klementyeva@mail.ru>
2025-03-21 17:30:14 +0700

    create book-card component, add routes for new component, change layout for books-list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { BooksListComponent } from './components/books-list/books-list.component';
+import { BookCardComponent } from './components/book-card/book-card.component';
 
 export const routes: Routes = [
-    {path: 'books', component: BooksListComponent},
-    {path: '', redirectTo: 'books', pathMatch: 'full'}
+    {path: 'books', title: 'Test task', component: BooksListComponent},
+    {path: 'book/:id', title: 'Test task | book card', component: BookCardComponent },
+    {path: '', redirectTo: 'books', pathMatch: 'full'},
 ];
--- src/app/components/books-list/books-list.component.ts
+++ src/app/components/books-list/books-list.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 import { BookItem, BooksService } from '../../books.service';
+import { TableModule } from 'primeng/table';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-books-list',
-  imports: [],
+  imports: [TableModule],
   templateUrl: './books-list.component.html',
-  styleUrl: './books-list.component.scss'
+  styleUrl: './books-list.component.scss',
 })
 export class BooksListComponent {
   booksList: BookItem[] = [];
+  headers: Array<string> = ['Название', 'Автор', 'Страницы', 'Язык', 'Жанр'];
 
-  constructor(private booksService: BooksService) {}
+  constructor(private booksService: BooksService, private router: Router) {}
 
-  ngOnInit():void {
+  ngOnInit(): void {
     this.booksService.getBooksList().subscribe((data) => {
       this.booksList = data.data;
     });
+  }
+
+  navigateToBookCard(id: number):void {
+    this.router.navigate(['book', id]);
   }
 }
