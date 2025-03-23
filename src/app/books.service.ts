3a364f95090abff4d8135c5c0baec5fad1c8e9af
Marina Kotova <m.klementyeva@mail.ru>
2025-03-23 16:15:24 +0700

    create updateBooksList method in service, which adds new book in booksList

--- src/app/books.service.ts
+++ src/app/books.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -19,10 +19,22 @@ export interface BookItem {
 export class BooksService {
 
   private apiUrl = 'https://api.jsonbin.io/v3/b/67dec4978561e97a50f0d1b1'; 
+  private apiKey = '$2a$10$R/dsxw9uMGjR80a5NChQSepitV7WyfuEwA1Fu3U2JQgC8qGTl9WVS'; 
 
   constructor(private http: HttpClient) {}
 
   getBooksList(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
+  }
+
+  updateBooksList(newBooksList: any) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'X-Master-Key': this.apiKey, 
+    });
+
+    return this.http.put(this.apiUrl, { data: newBooksList }, { headers }).subscribe(() => {
+      console.log('Данные успешно обновлены');
+    });
   }
 }
--- src/app/components/book-form/book-form.component.ts
+++ src/app/components/book-form/book-form.component.ts
@@ -8,8 +8,9 @@ import {
 import { ButtonModule } from 'primeng/button';
 import { BookItem, BooksService } from '../../books.service';
 import { map } from 'rxjs';
-import { SelectComponent } from "../select/select.component";
+import { SelectComponent } from '../select/select.component';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-form',
@@ -19,11 +20,17 @@ import { CommonModule } from '@angular/common';
 })
 export class BookFormComponent {
   form: FormGroup;
+  booksList: BookItem[] = [];
   authorsList: Array<string> = [];
   languagesList: Array<string> = [];
 
-  constructor(private fb: FormBuilder, private booksService: BooksService) {
+  constructor(
+    private fb: FormBuilder,
+    private booksService: BooksService,
+    private router: Router
+  ) {
     this.form = this.fb.group({
+      id: [null, Validators.required],
       name: ['', Validators.required],
       author: ['', Validators.required],
       pages: [null, [Validators.required, Validators.min(0)]],
@@ -34,26 +41,40 @@ export class BookFormComponent {
   }
 
   ngOnInit(): void {
-    this.booksService.getBooksList().pipe(
-      map((data) => {
-        const bookInfo = data.record.data.map((book: BookItem) => ({
-          author: book.author,
-          language: book.language,
-        }));
-  
-        return {
-          uniqueAuthors: [...new Set(bookInfo.map((item: {author: string}) => item.author))],
-          uniqueLanguages: [...new Set(bookInfo.map((item: {language: string}) => item.language))],
-        };
-      })
-    )
-    .subscribe(({ uniqueAuthors, uniqueLanguages }) => {
-      this.authorsList = uniqueAuthors as Array<string>;
-      this.languagesList = uniqueLanguages as Array<string>;
-    });
+    this.booksService
+      .getBooksList()
+      .pipe(
+        map((data) => {
+          this.booksList = data.record.data;
+          const bookInfo = data.record.data.map((book: BookItem) => ({
+            author: book.author,
+            language: book.language,
+          }));
+
+          return {
+            uniqueAuthors: [
+              ...new Set(
+                bookInfo.map((item: { author: string }) => item.author)
+              ),
+            ],
+            uniqueLanguages: [
+              ...new Set(
+                bookInfo.map((item: { language: string }) => item.language)
+              ),
+            ],
+          };
+        })
+      )
+      .subscribe(({ uniqueAuthors, uniqueLanguages }) => {
+        this.authorsList = uniqueAuthors as Array<string>;
+        this.languagesList = uniqueLanguages as Array<string>;
+        this.form.patchValue({ id: this.booksList.length + 1 });
+      });
   }
 
   onSubmit(value: BookItem) {
-    console.log(value);
+    this.booksList = [...this.booksList, value];
+    this.booksService.updateBooksList(this.booksList);
+    this.router.navigate(['books']);
   }
 }
